1c125651822a59cd59593ccc266ce15b249c8bfb
armando.dominguez <armando.dominguez@globant.com>
2016-03-07 15:55:46 -0600

    two services one for master, one for detail 1st version

--- api/controllers/CarteleraDiaController.js
+++ api/controllers/CarteleraDiaController.js
@@ -89,18 +89,19 @@ module.exports = {
 		 var request = require('request');
 		 var cheerio = require('cheerio');
 
-		 var url = 'http://www.cinetecanacional.net/controlador.php?opcion=carteleraDia';
+		 var url = 'http://www.cinetecanacional.net/'; 				 
 		 var dia = req.param('dia');
-		 var self = Array();
+		 var comando = 'controlador.php?opcion=carteleraDia';
+		 var peliculas = Array();
+		 var sinopsis = '';
 
 		 console.log('ENTER CONTROLLER');		 
 		 console.log('PARAM ARRIVED ['+dia+']');
 
-		 if(dia !== undefined){
-		 	url += '&dia='+dia;
-		 }		 
+		 undefined !== dia ? url += comando + '&dia=' + dia : url += comando;
 
-		 request({uri: url, enconding: 'utf8'}, function(err, response, body){
+		 
+		 request({uri: url, enconding: 'binary'}, function(err, response, body){
 		 	if(err && response.statusCode !== 200){
 		 		console.log('ERROR SERVER ['+err+']');
 		 	}else{
@@ -109,33 +110,68 @@ module.exports = {
 
 		 		$('#contenedorPelicula').each(function(i, item){
 
-		 			$title 	  = $(item).find('.peliculaTitulo'),
+		 			    $title 	  = $(item).find('.peliculaTitulo'),
 		 				$ficha 	  = $(item).find('.peliculaMiniFicha'),
 		 				$horarios = $(item).find('#horarios'),
 		 				$sinopsis = $(item).find('#peliculaSinopsis')
 		 										.children().remove().end(),
 		 				$urlImg	  = $(item).find('img'),
 		 				$urlDetail = $(item).find('#botonVer');
-		 				
 
-		 				self[i] = {
+		 				peliculas[i] = {
 		 					peliculaTitulo: 	$title.text().trim(),
 		 					peliculaMiniFicha:  $ficha.text().trim(),
 		 					peliculaSinopsis:   $sinopsis.text().trim(),
 		 					horarios: 			$horarios.text().trim(),
 		 					urlImg: 			$urlImg.attr('src'),
-		 					test: $urlDetail.attr('onclick'),
-
+		 					urlDetail: $urlDetail.attr('onclick').substring(15),		 					
 
 		 				};
 
 		 		});
 		 	
 				return res.json({
-					self
+					error: response.statusCode,
+					peliculas: peliculas
 				});
 		 	}		 	
 		 });
+	},
+
+	getDetail:function (req, res){
+
+		 var request = require('request');
+		 var cheerio = require('cheerio');
+		 var url = 'http://www.cinetecanacional.net/'; 
+		 var command = req.param('command');
+
+		 undefined !== command ?  url+=command : url;
+
+		 console.log('URL ['+url+']');
+		 console.log('COMMAND ['+command+']');
+
+		 request({url, encoding: 'binary'}, function(err, response, body){
+
+		 	if(err && response.statusCode !== 200){
+		 		console.log('ERROR 2'+err);
+		 	}else{
+
+		 		var $ = cheerio.load(body);		 		
+		 		$detalle = $('#peliculaSinopsis');
+		 		$avance = $('#botonTrailer');
+
+		 		console.log($avance.attr('onclick'));
+
+
+		 		return res.json({
+		 			error:response.statusCode,
+		 			sinopsisCompleta: $detalle.text().trim(),		 			
+		 			trailer: undefined !== $avance.attr('onclick') ? $avance.attr('onclick').match(/\/(.{11})(?:\'|\?)/)[1] : ''
+		 		});
+		 	}
+
+		 });
+
 	},
 
 	testXray:function(req, res){
